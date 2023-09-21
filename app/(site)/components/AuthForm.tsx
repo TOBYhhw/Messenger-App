d31eccbe20cb383347bc2ec0f1ade48445798c7d
TOBYhhw <1505994122@qq.com>
2023-09-21 16:15:13 +0800

    finish form

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Input from "@/app/components/inputs/Input";
-
+import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 type Variant = "LOGIN" | "REGISTER";
+
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
@@ -56,15 +59,70 @@ const AuthForm = () => {
         "
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
           <Input
-            disabled={isLoading}
+            id="email"
+            label="Email address"
+            type="email"
             register={register}
             errors={errors}
-            required
-            id="name"
-            label="Name"
           />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div
+                className="
+                  w-full 
+                  border-t
+                border-gray-300"
+              />
+            </div>
+            <div
+              className="
+            relative 
+            flex 
+            justify-center
+             text-sm
+             "
+            >
+              <span
+                className="
+              bg-white 
+              px-2 
+              text-gray-500
+              "
+              >
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className=" mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import clsx from "clsx";
+import { type } from "os";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
@@ -19,6 +20,7 @@ const Input: React.FC<InputProps> = ({
   register,
   required,
   errors,
+  type = "text",
   disabled,
 }) => {
   return (
@@ -35,6 +37,37 @@ const Input: React.FC<InputProps> = ({
       >
         {label}
       </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `
+            form-input
+            block 
+            w-full 
+            rounded-md 
+            border-0 
+            py-1.5 
+            text-gray-900 
+            shadow-sm 
+            ring-1 
+            ring-inset 
+            ring-gray-300 
+            placeholder:text-gray-400 
+            focus:ring-2 
+            focus:ring-inset 
+            focus:ring-sky-600 
+            sm:text-sm 
+            sm:leading-6`,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
     </div>
   );
 };
